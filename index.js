34bcc15169382caeb8eaee3e1062772f98b5a5db
Jeffchiucp <jeffchiu2000@gmail.com>
2017-08-28 16:47:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+const app = express();
+
+// Passport | using callbackURL
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access Token', accessToken);
+      console.log('refreshToken Token', refreshToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
